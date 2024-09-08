6fbe5c0f3f7486b62962cf01df745e1c8174bcf3
harramzahid <harramzahid5@gmail.com>
2024-09-08 12:55:26 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,13 @@
+// Coding Challenge: Bill Splitter Application
+
+//Task 1 calculating the tip based on bill size
+// array order of bills
+let Bill = [275, 40, 30]
+if (Bill = 50 <= X <= 300) { let tip = Bill*0.15;}
+else if ( Bill = X > 300 ) { let tip = Bill*0.2; }
+console.log("tip", tip);
+
+
+
+
+
