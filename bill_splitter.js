50d3aa3f8055a24439cbe30b543f771945a68d33
harramzahid <harramzahid5@gmail.com>
2024-09-08 13:27:27 -0400

    Add output functionality to display bill, tip, and total

--- bill_splitter.js
+++ bill_splitter.js
@@ -2,10 +2,15 @@
 
 //Task 1 calculating the tip based on bill size
 // array order of bills
-let Bill = [275, 40, 30]
+let Bill = [X]
 if (Bill = 50 <= X <= 300) { let tip = Bill*0.15;}
 else if ( Bill = X > 300 ) { let tip = Bill*0.2; }
 console.log("tip", tip);
+
+// Task  2 Output Details
+let totalbill = tip += Bill 
+console.log("tip += Bill", totalbill);
+console.log (`Thank you for your order, your bill was $$"Bill", the tip was $$"tip" and so, the total value is $$"totalbill" `);
 
 
 
