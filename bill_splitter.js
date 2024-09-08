d1df204352e3fe632f102cc2b5cc2a8c7c5ffc8b
harramzahid <harramzahid5@gmail.com>
2024-09-08 13:45:27 -0400

    Create and test calculateTip function

--- bill_splitter.js
+++ bill_splitter.js
@@ -1,8 +1,8 @@
 // Coding Challenge: Bill Splitter Application
 
 //Task 1 calculating the tip based on bill size
-// array order of bills
-let Bill = [X]
+// 
+var Bill = [X]
 if (Bill = 50 <= X <= 300) { let tip = Bill*0.15;}
 else if ( Bill = X > 300 ) { let tip = Bill*0.2; }
 console.log("tip", tip);
@@ -11,6 +11,17 @@ console.log("tip", tip);
 let totalbill = tip += Bill 
 console.log("tip += Bill", totalbill);
 console.log (`Thank you for your order, your bill was $$"Bill", the tip was $$"tip" and so, the total value is $$"totalbill" `);
+
+// Task 3 Create a function
+// Function Declaration - Calculate Tip 
+function calculatetip( Bill, tip) {
+    return tip; 
+} 
+
+let Bill = 100
+console.log(`tip when bill is $100: $${calculatetip(100, 15)}`);
+
+
 
 
 
